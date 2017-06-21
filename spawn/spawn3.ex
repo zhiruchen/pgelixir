fd5dcaa68e806aeff8e61c71db9f05ecf8b42bac
zhiruchen <zhiruchen1992@foxmail.com>
2017-06-21 22:29:09 +0800

    concurrent programming

---
+++ spawn/spawn3.ex
@@ -0,0 +1,23 @@
+defmodule Spawn3 do
+    def greet do
+        receive do
+            {sender, msg} ->
+                send sender, { :ok, "Hello  #{msg}" }
+        end
+    end
+end
+
+pid = spawn(Spawn3, :greet, [])
+send pid, {self(), "World!"}
+receive do
+    {:ok, msg} ->
+        IO.puts msg
+end
+
+send pid, {self(), "Elixir!"}
+receive do
+    {:ok, msg} ->
+        IO.puts msg
+    after 500 ->
+        IO.puts "the greeter is gone away"
+end
